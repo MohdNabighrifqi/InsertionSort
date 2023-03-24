06b0b9df670cf4dba9a1eea45f8e0f48191fadce
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-03-24 14:54:25 +0700

    menambahkan prosedur input

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
-using namespace std,
+using namespace std;
 
 int arr[20];
 int n;
+
+void input() {
+	while (true) {
+		cout << "masukan jumlah data pada array : ";
+		cin >> n;
+
+		if (n <= 20) {
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukan maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "=====================" << endl;
+	cout << "Masukan Element Array" << endl;
+	cout << "=====================" << endl;
+
+	for (int i = 0; i < n; i++) 
+	{
+		cout << "Data ke-" << (i + 1);
+	}
+}
