75e08186959fcbb58d213bcdc636724d9846cb66
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-03-24 15:18:06 +0700

    Menampilkan display

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -55,3 +55,13 @@ void insertionsort() {
 		
 	}
 }
+
+void display() {
+	cout << "=================================" << endl;
+	cout << "Element Array Yang Telah Tersusun" << endl;
+	cout << "=================================" << endl;
+
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << " ";
+	}
+}
